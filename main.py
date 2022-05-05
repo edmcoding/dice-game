bf9a9d8a1b4541c2bd259af4957b5f2392ba69a9
Edward May <104997498+edmcoding@users.noreply.github.com>
2022-05-05 14:55:44 +0000

    winning system started

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import random
 def diceroll():
   return (random.randint(1,6))
 
-
+player1 = diceroll()
 
 print (f"Player 1 rolls a {diceroll()}")
-       
+
+import random
+def diceroll2():
+  return (random.randint(1,6))
+
+player2 = diceroll2()
+
+print (f"Player 2 rolls a {diceroll2()}")
+
+if player1 == player2:
+    print("It's a draw!")
