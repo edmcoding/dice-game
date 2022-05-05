eb9144f3e50256d271fb0719166c9f53b1c8b48d
Edward May <104997498+edmcoding@users.noreply.github.com>
2022-05-05 15:03:10 +0000

    scoring system completed with bug

--- main.py
+++ main.py
@@ -15,4 +15,9 @@ player2 = diceroll2()
 print (f"Player 2 rolls a {diceroll2()}")
 
 if player1 == player2:
-    print("It's a draw!")
+  print("It's a draw!")
+elif player1 > player2:
+  print (f"Player 1 wins!) ({player1} over {player2}")
+else:
+  print (f"Player 2 wins! ({player2} over {player1})")
+         
